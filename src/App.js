97a3f17f197f2c8cd49a81cd67add216222503b5
allie <69830149+alliejang99@users.noreply.github.com>
2021-03-29 17:45:17 +0900

    2021.03.29

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Day from "./components/Day";
+import DayList from "./components/DayList";
+import Header from "./components/Header";
+import EmptyPage from "./components/EmptyPage";
+import CreateWord from "./components/CreateWord";
+import CreateDay from "./components/CreateDay";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <DayList />
+          </Route>
+          <Route path="/day/:day">
+            <Day />
+          </Route>
+          <Route path="/create_word">
+            <CreateWord />
+          </Route>
+          <Route path="/create_day">
+            <CreateDay />
+          </Route>
+          <Route>
+            <EmptyPage />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Header () {
+  return (
+    <div className="header">
+      <h1>
+        <Link to="/">토익 영단어 (고급)</Link>
+      </h1>
+      <div className="menu">
+        <Link to="/create_word" href="#word" className="link">단어 추가</Link>
+        <Link to="/create_day" href="#Day" className="link">Day 추가</Link>
+      </div>
+    </div>
+  );
+}
